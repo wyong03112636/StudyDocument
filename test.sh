c12055fbde2762c56546a353a5c1645201d474f7
wangyong-fe <wangyong-fe@travelsky.com.cn>
2023-02-16 13:06:23 +0800

    test: 触发流水线

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
-workdir=$(cd $(dirname $0); pwd)
-doctor="/.doctor.log"
-echo $workdir$doctor
-data=$(cat $workdir$doctor)
+workdir=$(pwd)"/.doctor.log"
+echo $workdir
+data=$(cat $workdir)
 echo $data
-if [ $data = 'true' ];then
-    node -v
-    npm -v
-    rm -rf node_modules
-    npm install
-    npm run build
-fi
+# if [ $data = 'true' ];then
+#     node -v
+#     npm -v
+#     rm -rf node_modules
+#     npm install
+#     npm run build
+# fi
